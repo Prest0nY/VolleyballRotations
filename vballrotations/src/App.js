59095dc7b0866e5ae91c26279ce0d0cb26e89421
Prest0nY <75341414+Prest0nY@users.noreply.github.com>
2024-01-13 11:59:10 -0800

    Added navbar and started program

---
+++ vballrotations/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Navbar.js"
+
+
+function App() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Volleyball Rotations</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vballrotations/src/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./App.css"
+
+function Navbar () {
+    return(
+        <div className="navbar">
+            <h3></h3>
+            <h3>Rotation 1</h3>
+            <h3>Rotation 2</h3>
+            <h3>Rotation 3</h3>
+            <h3>Rotation 4</h3>
+            <h3>Rotation 5</h3>
+            <h3>Rotation 6</h3>
+        </div>
+    )
+}
+
+export default Navbar
